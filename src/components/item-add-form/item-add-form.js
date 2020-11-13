3dc93a03d7526e6661d004bc7eaed5c85fc83c5a
Endzhe Galieva <endzhe91@gmail.com>
2020-11-13 19:34:14 +0300

    implement the filter

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -10,13 +10,16 @@ export default class ItemAddForm extends Component {
 
   onLabelChange = (e) => {
     this.setState({
-      label: e.target.value
+      label: e.target.value.toUpperCase()
     });
   };
 
   onSubmit = (e) => {
     e.preventDefault();
     this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ''
+    });
   };
  
   render() {
@@ -27,7 +30,8 @@ export default class ItemAddForm extends Component {
         <input type="text"
                className="form-control"
                onChange={this.onLabelChange}
-               placeholder="What needs to be done" />
+               placeholder="What needs to be done"
+               value={this.state.label}/>
         <button 
          className="btn btn-outline-secondary">
          Add Item
